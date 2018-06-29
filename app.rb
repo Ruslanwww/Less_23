0afefb64c23dde79061d823fb561cb43e4728fa1
Ruslanwww <oruslano51@gmail.com>
2018-06-30 00:55:39 +0300

    require sqlite3

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+configure do
+
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
